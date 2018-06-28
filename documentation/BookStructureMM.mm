2a7234598428f4aa2ef691f49b9e07b450319b53
nbesteman <nbesteman@gmail.com>
2018-06-28 14:19:20 -0400

    First commit

---
+++ documentation/BookStructureMM.mm
@@ -0,0 +1,335 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#009999" CREATED="1529517476207" ID="ID_1118779580" MODIFIED="1530189749806" TEXT="What We Do" VGAP="2">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      What We Do:&#160;&#160;This process is to be used and adapted to build the skills and abilities of the team members while strengthening the environment that we work in and improving the service.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="Tahoma" SIZE="20"/>
+<hook NAME="accessories/plugins/HierarchicalIcons.properties"/>
+<node COLOR="#cc33ff" CREATED="1529517476207" HGAP="59" ID="ID_844360277" MODIFIED="1530187990789" POSITION="right" TEXT="Brand" VSHIFT="777">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node COLOR="#3300ff" CREATED="1530104257113" HGAP="90" ID="ID_505485373" MODIFIED="1530109542090" TEXT="Awards" VSHIFT="-136">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node COLOR="#009999" CREATED="1530104266170" HGAP="40" ID="ID_680262672" MODIFIED="1530110198436" TEXT="GIS Champion" VSHIFT="-75">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node COLOR="#00cc00" CREATED="1530104285434" HGAP="118" ID="ID_220192891" MODIFIED="1530109585591" TEXT="GIS Champion Award Code" VSHIFT="-58">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="Dialog" SIZE="20"/>
+</node>
+<node CREATED="1530191474051" ID="ID_1332675720" MODIFIED="1530191518618" TEXT="List of Award Recipients by date">
+<node CREATED="1529504592976" ID="ID_1136323314" MODIFIED="1529504603092" TEXT="Eq - Ian"/>
+<node CREATED="1529504582435" ID="ID_328525826" MODIFIED="1529504588242" TEXT="E.H. - Nancy"/>
+</node>
+</node>
+<node CREATED="1530192157520" ID="ID_1358991753" MODIFIED="1530192950075" TEXT="ex"/>
+</node>
+<node COLOR="#3300ff" CREATED="1530104257113" HGAP="90" ID="ID_532337857" MODIFIED="1530191377253" TEXT="Social Media" VSHIFT="-136">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node CREATED="1529507731565" ID="ID_198567546" MODIFIED="1529507739090" TEXT="GVSU Alumni"/>
+<node CREATED="1529507590888" ID="ID_729106599" MODIFIED="1529507595606" TEXT="LinkedIn"/>
+<node CREATED="1529505878767" ID="ID_1762647964" MODIFIED="1529505883587" TEXT="Watercooler">
+<node CREATED="1529507346247" ID="ID_1825773858" MODIFIED="1529507384437" TEXT="post the GIS Champion certificate"/>
+</node>
+</node>
+<node CREATED="1529507660711" ID="ID_677571628" MODIFIED="1529507668220" TEXT="Articles"/>
+<node CREATED="1529507668788" ID="ID_178389402" MODIFIED="1530192029979" TEXT="Map Contests">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      https://www.gislounge.com/gis-and-map-contests-roundup/
+    </p>
+    <p>
+      https://cartogis.org/award/map-design-competition/
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1529505954406" ID="ID_371925706" MODIFIED="1530191658746" TEXT="Outreach">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      outreach - go talk to people
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1530191661980" ID="ID_186312752" MODIFIED="1530192139165" TEXT="sneakernet - go talk to people"/>
+<node CREATED="1530191667800" ID="ID_764277567" MODIFIED="1530191712434" TEXT="email contact with Twps">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      list of local units who does what and last contact date
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1529504615036" ID="ID_1551658875" MODIFIED="1530191621834" TEXT="State GIS">
+<node CREATED="1529504628546" ID="ID_72370746" MODIFIED="1529504656429" TEXT="next meeting is Thurs Aug 2 at 9:30 a.m."/>
+</node>
+</node>
+</node>
+<node COLOR="#cc33ff" CREATED="1530103755143" HGAP="53" ID="ID_1778119501" MODIFIED="1530187998852" POSITION="right" TEXT="Methods" VSHIFT="-13">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node COLOR="#3300ff" CREATED="1530103998762" HGAP="66" ID="ID_857853392" MODIFIED="1530191348934" TEXT="Documentation" VSHIFT="-252">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      We document the concepts in this book.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node COLOR="#009999" CREATED="1530104050970" HGAP="41" ID="ID_1705539748" MODIFIED="1530110218638" TEXT="About Documentation" VSHIFT="-14">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node COLOR="#00cc00" CREATED="1530104130025" HGAP="71" ID="ID_1421272672" MODIFIED="1530109312796" TEXT="How This Book Works" VSHIFT="-47">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="Dialog" SIZE="20"/>
+</node>
+</node>
+<node COLOR="#009999" CREATED="1530104013945" HGAP="52" ID="ID_1153044282" MODIFIED="1530110198436" TEXT="Documents Storage Concepts" VSHIFT="-31">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node COLOR="#00cc00" CREATED="1530104098825" HGAP="26" ID="ID_231105376" MODIFIED="1530191095780" TEXT="GIS File Standard" VSHIFT="-40">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Paragraph describing our file structure.
+    </p>
+    <p>
+      Paragraph describing file structure for MapInfo.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="Dialog" SIZE="20"/>
+</node>
+<node COLOR="#00cc00" CREATED="1530104098825" HGAP="26" ID="ID_1329901202" MODIFIED="1530191116679" TEXT="GitHub" VSHIFT="-40">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      We have explored this but are weak in this area.&#160;&#160;We need to start pushing all code to Github.&#160;&#160;Security needs to be kept in mind at all times.
+    </p>
+    <p>
+      This needs to be flushed out more.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="Dialog" SIZE="20"/>
+</node>
+</node>
+<node COLOR="#009999" CREATED="1530104013945" HGAP="52" ID="ID_1509414048" MODIFIED="1530188492072" TEXT="Product Documentation" VSHIFT="-31">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node COLOR="#00cc00" CREATED="1530104098825" FOLDED="true" HGAP="26" ID="ID_1460089185" MODIFIED="1530191044764" TEXT="Product Model" VSHIFT="-40">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(34, 34, 34)" face="Roboto, arial, sans-serif">We have a model that we want our product documentation to look like.&#160; The Poplar Hill Cemetery Management Program is currently that model. </font>
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      <font color="rgb(34, 34, 34)" face="Roboto, arial, sans-serif">A&#160;</font><font color="rgb(34, 34, 34)" face="Roboto, arial, sans-serif" size="16px"><b style="color: rgb(34, 34, 34); font-family: Roboto, arial, sans-serif; font-size: 16px; font-style: normal; letter-spacing: normal; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)">template</b></font><font color="rgb(34, 34, 34)" face="Roboto, arial, sans-serif">&#160;is a pattern or mold: something you fill in to produce a finished product. </font>
+    </p>
+    <p>
+      <font color="rgb(34, 34, 34)" face="Roboto, arial, sans-serif">A&#160;</font><font color="rgb(34, 34, 34)" face="Roboto, arial, sans-serif" size="16px"><b style="color: rgb(34, 34, 34); font-family: Roboto, arial, sans-serif; font-size: 16px; font-style: normal; letter-spacing: normal; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)">model</b></font><font color="rgb(34, 34, 34)" face="Roboto, arial, sans-serif">&#160;is a finished product similar to what you're looking to produce.</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+<arrowlink DESTINATION="ID_1460089185" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_790825268" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<arrowlink DESTINATION="ID_1460089185" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_921923511" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1460089185" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_790825268" SOURCE="ID_1460089185" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1460089185" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_921923511" SOURCE="ID_1460089185" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<font BOLD="true" NAME="Dialog" SIZE="20"/>
+<node CREATED="1530189792359" ID="ID_1237202686" MODIFIED="1530190390593" TEXT="Poplar Hill Cemetery Management Program">
+<node CREATED="1530189858160" ID="ID_1520307663" MODIFIED="1530189865738" TEXT="Title and approval page"/>
+<node CREATED="1530189866290" ID="ID_131261994" MODIFIED="1530189885023" TEXT="Table of Contents"/>
+<node CREATED="1530189885620" ID="ID_927959438" MODIFIED="1530189896559" TEXT="Problem and Analysis">
+<node CREATED="1530189971302" ID="ID_1454897015" MODIFIED="1530189979334" TEXT="Statement of Problem"/>
+<node CREATED="1530189980001" ID="ID_471161342" MODIFIED="1530189997287" TEXT="Analysis"/>
+</node>
+<node CREATED="1530189897299" ID="ID_302144754" MODIFIED="1530189901775" TEXT="Design">
+<node CREATED="1530189998775" ID="ID_580261421" MODIFIED="1530190061548" TEXT="Program and Data Flow description">
+<node CREATED="1530190017558" ID="ID_913161581" MODIFIED="1530190024276" TEXT="Data Flow Chart"/>
+</node>
+<node CREATED="1530190024779" ID="ID_1538051139" MODIFIED="1530190072195" TEXT="User Navigation description">
+<node CREATED="1530190041659" ID="ID_1711030904" MODIFIED="1530190101662" TEXT="Function Navigation Flow Chart"/>
+</node>
+<node CREATED="1530190117043" ID="ID_781139641" MODIFIED="1530190125960" TEXT="Application Results"/>
+</node>
+<node CREATED="1530189903255" ID="ID_1092986864" MODIFIED="1530190220772" TEXT="Hard Copy Record">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      screenshots of each user and admin page, reports and tables
+    </p>
+  </body>
+</html>
+</richcontent>
+<node CREATED="1530190205236" ID="ID_1607077262" MODIFIED="1530190246216" TEXT="Introduction Splash Screen"/>
+<node CREATED="1530190246763" ID="ID_400913229" MODIFIED="1530190264431" TEXT="Control Panel"/>
+<node CREATED="1530189931860" ID="ID_677182190" MODIFIED="1530190296473" TEXT="Reports"/>
+<node CREATED="1530190297194" ID="ID_632179765" MODIFIED="1530190315557" TEXT="Tables"/>
+<node CREATED="1530190316050" ID="ID_1299105990" MODIFIED="1530190320348" TEXT="Queries"/>
+<node CREATED="1530190321284" ID="ID_1099013471" MODIFIED="1530190325558" TEXT="Relationships"/>
+</node>
+<node CREATED="1530189910021" ID="ID_1933884019" MODIFIED="1530189914510" TEXT="User Manual"/>
+<node CREATED="1530189914961" ID="ID_134128418" MODIFIED="1530189925638" TEXT="Code"/>
+</node>
+</node>
+<node COLOR="#00cc00" CREATED="1530104098825" FOLDED="true" HGAP="26" ID="ID_1623711650" MODIFIED="1530191043047" TEXT="Product Template" VSHIFT="-40">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(34, 34, 34)" face="Roboto, arial, sans-serif">A&#160;</font><font color="rgb(34, 34, 34)" face="Roboto, arial, sans-serif" size="16px"><b style="color: rgb(34, 34, 34); font-family: Roboto, arial, sans-serif; font-size: 16px; font-style: normal; letter-spacing: normal; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)">template</b></font><font color="rgb(34, 34, 34)" face="Roboto, arial, sans-serif">&#160;is a pattern or mold: something you fill in to produce a finished product. </font>
+    </p>
+    <p>
+      <font color="rgb(34, 34, 34)" face="Roboto, arial, sans-serif">A&#160;</font><font color="rgb(34, 34, 34)" face="Roboto, arial, sans-serif" size="16px"><b style="color: rgb(34, 34, 34); font-family: Roboto, arial, sans-serif; font-size: 16px; font-style: normal; letter-spacing: normal; text-align: left; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px; background-color: rgb(255, 255, 255)">model</b></font><font color="rgb(34, 34, 34)" face="Roboto, arial, sans-serif">&#160;is a finished product similar to what you're looking to produce. </font>
+    </p>
+    <p>
+      
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+<arrowlink DESTINATION="ID_1623711650" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_347391665" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<arrowlink DESTINATION="ID_1623711650" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1877349623" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1623711650" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_347391665" SOURCE="ID_1623711650" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<linktarget COLOR="#b0b0b0" DESTINATION="ID_1623711650" ENDARROW="Default" ENDINCLINATION="0;0;" ID="Arrow_ID_1877349623" SOURCE="ID_1623711650" STARTARROW="None" STARTINCLINATION="0;0;"/>
+<font BOLD="true" NAME="Dialog" SIZE="20"/>
+<node CREATED="1530190414400" ID="ID_95762115" MODIFIED="1530190435598" TEXT="Stripped out model.  Framework to start with.">
+<node CREATED="1530189858160" ID="ID_1954842524" MODIFIED="1530189865738" TEXT="Title and approval page"/>
+<node CREATED="1530189866290" ID="ID_1646361214" MODIFIED="1530189885023" TEXT="Table of Contents"/>
+<node CREATED="1530189885620" ID="ID_63558272" MODIFIED="1530189896559" TEXT="Problem and Analysis">
+<node CREATED="1530189971302" ID="ID_898347760" MODIFIED="1530189979334" TEXT="Statement of Problem"/>
+<node CREATED="1530189980001" ID="ID_286092902" MODIFIED="1530189997287" TEXT="Analysis"/>
+</node>
+<node CREATED="1530189897299" ID="ID_764099384" MODIFIED="1530189901775" TEXT="Design">
+<node CREATED="1530189998775" ID="ID_1943937610" MODIFIED="1530190061548" TEXT="Program and Data Flow description">
+<node CREATED="1530190017558" ID="ID_991294004" MODIFIED="1530190024276" TEXT="Data Flow Chart"/>
+</node>
+<node CREATED="1530190024779" ID="ID_387418607" MODIFIED="1530190072195" TEXT="User Navigation description">
+<node CREATED="1530190041659" ID="ID_1623015798" MODIFIED="1530190101662" TEXT="Function Navigation Flow Chart"/>
+</node>
+<node CREATED="1530190117043" ID="ID_80424912" MODIFIED="1530190125960" TEXT="Application Results"/>
+</node>
+<node CREATED="1530189903255" ID="ID_1412172569" MODIFIED="1530190220772" TEXT="Hard Copy Record">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      screenshots of each user and admin page, reports and tables
+    </p>
+  </body>
+</html></richcontent>
+<node CREATED="1530190205236" ID="ID_110052113" MODIFIED="1530190246216" TEXT="Introduction Splash Screen"/>
+<node CREATED="1530190246763" ID="ID_1650657993" MODIFIED="1530190264431" TEXT="Control Panel"/>
+<node CREATED="1530189931860" ID="ID_1989125135" MODIFIED="1530190296473" TEXT="Reports"/>
+<node CREATED="1530190297194" ID="ID_1132708906" MODIFIED="1530190315557" TEXT="Tables"/>
+<node CREATED="1530190316050" ID="ID_697706743" MODIFIED="1530190320348" TEXT="Queries"/>
+<node CREATED="1530190321284" ID="ID_778652567" MODIFIED="1530190325558" TEXT="Relationships"/>
+</node>
+<node CREATED="1530189910021" ID="ID_1062430166" MODIFIED="1530189914510" TEXT="User Manual"/>
+<node CREATED="1530189914961" ID="ID_1336931371" MODIFIED="1530189925638" TEXT="Code"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#009999" CREATED="1530104072441" HGAP="39" ID="ID_1165004208" MODIFIED="1530110198436" TEXT="Team Concept" VSHIFT="55">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node COLOR="#00cc00" CREATED="1530104169929" HGAP="68" ID="ID_521471839" MODIFIED="1530190891714" TEXT="Paired Programming" VSHIFT="45">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Paragraph describing the concept from Joy Inc.
+    </p>
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="Dialog" SIZE="20"/>
+</node>
+</node>
+</node>
+<node COLOR="#cc00ff" CREATED="1530103778763" HGAP="37" ID="ID_1932581142" MODIFIED="1530110130509" POSITION="right" TEXT="Service" VSHIFT="-557">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node COLOR="#3300ff" CREATED="1529522408527" HGAP="88" ID="ID_943497094" MODIFIED="1530108803313" TEXT="Tools" VSHIFT="111">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node COLOR="#009999" CREATED="1529522449799" HGAP="149" ID="ID_873178899" MODIFIED="1530110198436" TEXT="ESRI Tools" VSHIFT="-54">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node COLOR="#00cc00" CREATED="1530103943369" HGAP="108" ID="ID_845617132" MODIFIED="1530110067348" TEXT="Using COGO Tools in ArcGIS" VSHIFT="-124">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="Dialog" SIZE="20"/>
+</node>
+</node>
+<node COLOR="#009999" CREATED="1529522423919" HGAP="148" ID="ID_69661875" MODIFIED="1530110198436" TEXT="QGIS Tools" VSHIFT="52">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node COLOR="#00cc00" CREATED="1530103902871" HGAP="95" ID="ID_1449241727" MODIFIED="1530108947689" TEXT="Using COGO Tools in QGIS" VSHIFT="-85">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="Dialog" SIZE="20"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
---
+++ documentation/BookStructureMM_20180628.mm
@@ -0,0 +1,80 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#009999" CREATED="1529517476207" ID="ID_1118779580" MODIFIED="1530110198436" TEXT="What We Do" VGAP="2">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="Tahoma" SIZE="20"/>
+<hook NAME="accessories/plugins/HierarchicalIcons.properties"/>
+<node COLOR="#cc33ff" CREATED="1529517476207" FOLDED="true" HGAP="59" ID="ID_844360277" MODIFIED="1530110240982" POSITION="right" TEXT="Brand" VSHIFT="777">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node COLOR="#3300ff" CREATED="1530104257113" HGAP="90" ID="ID_505485373" MODIFIED="1530109542090" TEXT="Awards" VSHIFT="-136">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node COLOR="#009999" CREATED="1530104266170" HGAP="40" ID="ID_680262672" MODIFIED="1530110198436" TEXT="GIS Champion" VSHIFT="-75">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node COLOR="#00cc00" CREATED="1530104285434" HGAP="118" ID="ID_220192891" MODIFIED="1530109585591" TEXT="GIS Champion Award Code" VSHIFT="-58">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="Source Sans Pro Black" SIZE="20"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#cc33ff" CREATED="1530103755143" FOLDED="true" HGAP="53" ID="ID_1778119501" MODIFIED="1530110245654" POSITION="right" TEXT="Methods" VSHIFT="-13">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node COLOR="#3300ff" CREATED="1530103998762" HGAP="66" ID="ID_857853392" MODIFIED="1530110155715" TEXT="Documentation" VSHIFT="-252">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node COLOR="#009999" CREATED="1530104050970" HGAP="41" ID="ID_1705539748" MODIFIED="1530110218638" TEXT="About Documentation" VSHIFT="-14">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node COLOR="#00cc00" CREATED="1530104130025" HGAP="71" ID="ID_1421272672" MODIFIED="1530109312796" TEXT="How This Book Works" VSHIFT="-47">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="Source Sans Pro Black" SIZE="20"/>
+</node>
+</node>
+<node COLOR="#009999" CREATED="1530104013945" HGAP="52" ID="ID_1153044282" MODIFIED="1530110198436" TEXT="Documents Storage Concepts" VSHIFT="-31">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node COLOR="#00cc00" CREATED="1530104098825" HGAP="26" ID="ID_231105376" MODIFIED="1530109874983" TEXT="GIS File Standard" VSHIFT="-40">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="Source Sans Pro Black" SIZE="20"/>
+</node>
+</node>
+<node COLOR="#009999" CREATED="1530104072441" HGAP="39" ID="ID_1165004208" MODIFIED="1530110198436" TEXT="Team Concept" VSHIFT="55">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node COLOR="#00cc00" CREATED="1530104169929" HGAP="68" ID="ID_521471839" MODIFIED="1530109991359" TEXT="Paired Programming" VSHIFT="45">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="Source Sans Pro Black" SIZE="20"/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#cc00ff" CREATED="1530103778763" HGAP="37" ID="ID_1932581142" MODIFIED="1530110130509" POSITION="right" TEXT="Service" VSHIFT="-557">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="18"/>
+<node COLOR="#3300ff" CREATED="1529522408527" HGAP="88" ID="ID_943497094" MODIFIED="1530108803313" TEXT="Tools" VSHIFT="111">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node COLOR="#009999" CREATED="1529522449799" HGAP="149" ID="ID_873178899" MODIFIED="1530110198436" TEXT="ESRI Tools" VSHIFT="-54">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node COLOR="#00cc00" CREATED="1530103943369" HGAP="108" ID="ID_845617132" MODIFIED="1530110067348" TEXT="Using COGO Tools in ArcGIS" VSHIFT="-124">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="Source Sans Pro Black" SIZE="20"/>
+</node>
+</node>
+<node COLOR="#009999" CREATED="1529522423919" HGAP="148" ID="ID_69661875" MODIFIED="1530110198436" TEXT="QGIS Tools" VSHIFT="52">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="14"/>
+<node COLOR="#00cc00" CREATED="1530103902871" HGAP="95" ID="ID_1449241727" MODIFIED="1530108947689" TEXT="Using COGO Tools in QGIS" VSHIFT="-85">
+<edge COLOR="#111111" WIDTH="thin"/>
+<font BOLD="true" NAME="Source Sans Pro Black" SIZE="20"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
