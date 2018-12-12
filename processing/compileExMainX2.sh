145500c3279afe16f8544df149996afe14637088
Bryan May <BMay@ALLEGANCOUNTY.ORG>
2018-12-12 16:37:56 -0500

    new overall structure

--- processing/compileExMainX2.sh
+++
@@ -1,2 +0,0 @@
-pdflatex -output-directory=build -synctex=1 -interaction=nonstopmode GISDocumentationExp.tex
-pdflatex -output-directory=build -synctex=1 -interaction=nonstopmode GISDocumentationExp.tex
